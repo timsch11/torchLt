ab0cf55e1c74aa4a44046508fefc66f9d4b4f3c6
timsch11 <tis4477@thi.de>
2024-11-28 16:21:22 +0100

    minor changes

--- cuda/util.cu
+++ cuda/util.cu
@@ -16,6 +16,7 @@ std::pair<unsigned int, unsigned int> computeBlockThreadAllocation(unsigned int 
 // error checking
 #define CHECK_CUDA_ERROR(val) check((val), #val, __FILE__, __LINE__)
 
+
 template <typename T>
 void check(T err, const char* const func, const char* const file, const int line) {
     if (err != cudaSuccess) {
