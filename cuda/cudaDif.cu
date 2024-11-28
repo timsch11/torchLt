0d7185a4ed5c310558f7e906d0ef27675816a060
timsch11 <tis4477@thi.de>
2024-11-28 16:20:10 +0100

    added relu derivative

--- cuda/cudaDif.cu
+++ cuda/cudaDif.cu
@@ -1,4 +1,5 @@
-#include "cudaMath.cu"
+//#include "cudaMath.cu"
+#include "util.cu"
 
 
 __global__ void reluGrad_kernel(float* targetMemorySpace, float* vector) {
@@ -12,8 +13,23 @@ __global__ void reluGrad_kernel(float* targetMemorySpace, float* vector) {
 
 cudaError_t reluGrad(float* targetMemorySpace, float* vector, unsigned int size) {
     std::pair<unsigned int, unsigned int> blocksThreads = computeBlockThreadAllocation(size);
-    reluGrad_kernel<<<blocksThreads.first, blocksThreads.second>>>(targetMemorySpace, vector);
-    CHECK_CUDA_ERROR(cudaDeviceSynchronize());
+    reluGrad_kernel<<<blocksThreads.first, blocksThreads.second, 0, 0>>>(targetMemorySpace, vector);
     CHECK_CUDA_ERROR(cudaGetLastError());
     return cudaSuccess;
+}
+
+
+int main() {
+    float h_bias[3] = {1.0f, -2.0f, 3.0f};
+    float *bias;
+    cudaMalloc(&bias, 3 * sizeof(float));
+    cudaMemcpy(bias, h_bias, 3 * sizeof(float), cudaMemcpyHostToDevice);
+
+    reluGrad(bias, bias, 3);
+
+    cudaMemcpy(h_bias, bias, 3*sizeof(float), cudaMemcpyDeviceToHost);
+
+    for (int i=0; i<3; i++) {
+        std::cout << h_bias[i] << " ";
+    }
 }
