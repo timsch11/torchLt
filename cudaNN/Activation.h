462f3abbe3dd008be0da4d28954b2786d05adc5c
timsch11 <109914479+timsch11@users.noreply.github.com>
2024-12-24 12:40:34 +0100

    Delete cudaNN directory

--- cudaNN/Activation.h
+++
@@ -1,9 +0,0 @@
-#include "Layer.h"
-
-class Activation : public Layer {
-    public:
-        Activation(int in_features);
-        void forward(float* input, float* output);
-        void backward(float* input, float* output);
-        void update(float* input, float* output);
-};
--- cudaNN/Layer.cu
+++
@@ -1,9 +0,0 @@
-#include "Layer.h"
-#include "../cuda/Tensor.h"
-
-
-    Layer::Layer(unsigned int in_features, unsigned int out_features) {
-        this->in_features = in_features;
-        this->out_features = out_features;
-        this->last_input = reserveMemoryOnDevice(in_features);
-    }
