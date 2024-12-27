b3de51c6e636cc61853f8c80e6006ea4ad5cc12e
timsch11 <tis4477@thi.de>
2024-12-27 16:56:30 +0100

    minor fixes

--- core/cuda/util.h
+++ core/cuda/util.h
@@ -20,13 +20,11 @@ std::pair<unsigned int, unsigned int> computeBlockThreadAllocation(unsigned int 
 // error checking
 #define CHECK_CUDA_ERROR(val) check((val), #val, __FILE__, __LINE__)
 
-
 template <typename T>
 void check(T err, const char* const func, const char* const file, const int line) {
     if (err != cudaSuccess) {
-        std::cout << "CUDA error during execution of" << std::string(func) << ":\nline: " << std::string(line) << 
         fprintf(stderr, "CUDA error at %s:%d code=%d(%s) \"%s\" \n", file, line, static_cast<unsigned int>(err), cudaGetErrorString(err), func);
-        throw std::runtime_error(std::string(cudaGetErrorString(err)));
+        exit(EXIT_FAILURE);
     }
 }
 
