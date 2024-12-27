deaa46e5c0b7ef6fedefa84a7f16cfa24a1150ef
timsch11 <tis4477@thi.de>
2024-12-27 15:19:42 +0100

    restructuring

---
+++ built.ps1
@@ -0,0 +1,19 @@
+# Make built directory
+mkdir built
+cd built 
+mkdir Tensorlib
+cd ..
+
+# Compile source files to objects
+$INCLUDE_PATH = "/core"
+
+nvcc -c core/cuda/util.cu -o built/Tensorlib/util.obj -I"$INCLUDE_PATH"
+nvcc -c core/Factory.cu -o built/Tensorlib/factory.obj -I"$INCLUDE_PATH"
+nvcc -c core/Tensor.cu -o built/Tensorlib/tensor.obj -I"$INCLUDE_PATH"
+nvcc -c core/cuda/cudaDif.cu -o built/Tensorlib/cudadif.obj -I"$INCLUDE_PATH"
+nvcc -c core/cuda/cudaMath.cu -o built/Tensorlib/cudamath.obj -I"$INCLUDE_PATH"
+nvcc -c core/cuda/cudaMem.cu -o built/Tensorlib/cudamem.obj -I"$INCLUDE_PATH"
+nvcc -c core/cuda/cudaNN.cu -o built/Tensorlib/cudann.obj -I"$INCLUDE_PATH"
+
+# Create static library
+nvcc -lib built/Tensorlib/util.obj built/Tensorlib/factory.obj built/Tensorlib/tensor.obj built/Tensorlib/cudadif.obj built/Tensorlib/cudamath.obj built/Tensorlib/cudamem.obj built/Tensorlib/cudann.obj -o Tensor.lib
